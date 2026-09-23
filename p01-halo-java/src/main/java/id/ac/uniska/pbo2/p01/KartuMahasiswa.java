c76552e9430ca613c6ef27731bfbd9bbd53dd86f
Muhammad Diva Saputra <projectdiput@gmail.com>
2026-09-23 19:18:14 +0700

    P01 Latihan Mandiri: Kartu Mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author medip
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String [] args) {
+        
+        String nama = "Muhammad Diva Saputra";
+        String npm = "2410010361";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "melanjutkan pbo 1";
+
+        System.out.println("=======================");
+        System.out.println("Kartu Mahasiswa PBO2");
+        System.out.println("========================");
+        System.out.println("nama     = " + nama);
+        System.out.println("npm      = " + npm);
+        System.out.println("prodi    = " + prodi);
+        System.out.println("semester = " + semester);
+        System.out.println("alasan   = " + alasan);
+        System.out.println("========================");
+    }    
+}
